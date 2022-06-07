7a7fd4533ba802ba54c787355e2afb9ab0f2d67b
Holworth <kangqihan17@mails.ucas.ac.cn>
2022-06-07 07:39:32 +0000

    Format assertion failed message

--- main.cc
+++ main.cc
@@ -19,8 +19,10 @@
 {\
   if ((val) != (expect)) \
   {\
-    std::cerr << msg << "\nexpect: " << expect << "\ngot: " << val << std::endl;\
-    std::cerr << strerror(errno) << std::endl;\
+    std::printf("[ASSERTION FAILED] at %s, %d\n", __FILE__, __LINE__);\
+    std::cerr << "[Error Msg]: " << msg << \
+                 "\n[EXPECT]: " << expect << "\n[GOT]: " << val << \
+                 "\n[Errno]: " << std::strerror(errno) << std::endl;\
     exit(1); \
   }\
 }
