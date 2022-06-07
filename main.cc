9c91a245ef99c49aa5f75b1a87aac3a9188e37d5
Holworth <kangqihan17@mails.ucas.ac.cn>
2022-06-07 06:52:42 +0000

    Add chroot

--- main.cc
+++ main.cc
@@ -5,10 +5,22 @@
 #include <cstdlib>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/mount.h>
+#include <sys/syscall.h>
+#include <sys/stat.h>
 #include <thread>
 #include <errno.h>
 #include <unistd.h>
-#include <sys/mount.h>
+#include <filesystem>
+#include <iostream>
+
+#define ASSERT_CHECK(val, expect, msg) \
+{\
+  if ((val) != (expect)) \
+  {\
+    std::cerr << msg << "\nexpect: " << expect << "\ngot: " << val;\
+  }\
+}
 
 const size_t kStackSize = 4 * 1024 * 1024;
 
@@ -16,14 +28,38 @@ void container_init() {
   mount("proc", "/proc", "proc", MS_NOEXEC | MS_NOSUID | MS_NODEV, nullptr);
 }
 
-int child_process(void* param) {
+void pivotRoot(const std::string& root) {
+  auto old_root = root + "/.pivot_root";
+
+  auto syscall_ret = mkdir(old_root.c_str(), 0777);
+  ASSERT_CHECK(syscall_ret, 0, "Create directory failed");
+
+  // Old root is mounted to old_root directory
+  syscall_ret = pivot_root(root.c_str(), old_root.c_str());
+  ASSERT_CHECK(syscall_ret, 0, "pivot root call failed");
+
+  // change current working directory to new root
+  syscall_ret = chdir("/");
+  ASSERT_CHECK(syscall_ret, 0, "chdir call failed");
+
+  // Unmount old root
+  syscall_ret = umount2("/.pivot_root", MNT_DETACH);
+  ASSERT_CHECK(syscall_ret, 0, "unmount failed");
+
+  // Remove old root
+  syscall_ret = rmdir("/.pivot_root");
+  ASSERT_CHECK(syscall_ret, 0, "remove directory failed");
+}
+
+int container_process(void* param) {
   (void)param;
   std::printf("Hello, World\n");
   std::printf("My pid is %d\n", getpid());
 
-  // Do some necessary initiate work
+  // Container initialization
   container_init();
 
+  // Open bash
   char* args[] = {"/bin/bash", nullptr};
   execv(args[0], args);
 
@@ -34,7 +70,7 @@ int main() {
   auto stack = malloc(kStackSize);
   auto clone_flags = CLONE_NEWUTS | CLONE_NEWPID | CLONE_NEWNS;
   auto flags = clone_flags | SIGCHLD;
-  auto pid = clone(child_process, (char*)stack + kStackSize, flags, nullptr);
+  auto pid = clone(container_process, (char*)stack + kStackSize, flags, nullptr);
   if (pid < 0) {
     std::printf("Failed to create child process");
     exit(1);
