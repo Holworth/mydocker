01bf28cc805e837b291a7c528cc95fc9ed65d4f8
Holworth <kangqihan17@mails.ucas.ac.cn>
2022-06-07 07:33:18 +0000

    rewrite pivot root

--- main.cc
+++ main.cc
@@ -3,6 +3,7 @@
 #include <sched.h>
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/mount.h>
@@ -18,24 +19,42 @@
 {\
   if ((val) != (expect)) \
   {\
-    std::cerr << msg << "\nexpect: " << expect << "\ngot: " << val;\
+    std::cerr << msg << "\nexpect: " << expect << "\ngot: " << val << std::endl;\
+    std::cerr << strerror(errno) << std::endl;\
+    exit(1); \
   }\
 }
 
+void pivotRoot(const std::string& root);
+
 const size_t kStackSize = 4 * 1024 * 1024;
 
-void container_init() {
-  mount("proc", "/proc", "proc", MS_NOEXEC | MS_NOSUID | MS_NODEV, nullptr);
+// Initialization parameters to booting a container
+struct CTParams {
+  std::string root_fs_dir;  // The absolute path for rootfs of docker image
+};
+
+void container_init(const CTParams* init_params) {
+  pivotRoot(init_params->root_fs_dir);
+  auto syscall_ret = mount("proc", "/proc", "proc", MS_NOEXEC | MS_NOSUID | MS_NODEV, nullptr);
+  ASSERT_CHECK(syscall_ret, 0, "mount proc file system failed");
+}
+
+int pivot_root(const std::string& new_root, const std::string& old_root) {
+  return syscall(SYS_pivot_root, new_root.c_str(), old_root.c_str());
 }
 
 void pivotRoot(const std::string& root) {
   auto old_root = root + "/.pivot_root";
 
-  auto syscall_ret = mkdir(old_root.c_str(), 0777);
+  auto syscall_ret = mount(root.c_str(), root.c_str(), "bind", MS_BIND | MS_REC, nullptr);
+  ASSERT_CHECK(syscall_ret, 0, "remount failed");
+
+  syscall_ret = mkdir(old_root.c_str(), 0777);
   ASSERT_CHECK(syscall_ret, 0, "Create directory failed");
 
   // Old root is mounted to old_root directory
-  syscall_ret = pivot_root(root.c_str(), old_root.c_str());
+  syscall_ret = pivot_root(root, old_root);
   ASSERT_CHECK(syscall_ret, 0, "pivot root call failed");
 
   // change current working directory to new root
@@ -56,23 +75,35 @@ int container_process(void* param) {
   std::printf("Hello, World\n");
   std::printf("My pid is %d\n", getpid());
 
-  // Container initialization
-  container_init();
+  auto init_param = reinterpret_cast<const CTParams*>(param);
 
-  // Open bash
+  // Container initialization
+  container_init(init_param);
+
+  // Run in bash
   char* args[] = {"/bin/bash", nullptr};
   execv(args[0], args);
 
   return 0;
 }
 
-int main() {
+void parse_parameters(CTParams* params, int argc, char* argv[]) {
+  ASSERT_CHECK((argc > 1), true, "Require at least one argument");
+  params->root_fs_dir = std::string(argv[1]);
+}
+
+int main(int argc, char* argv[]) {
+  CTParams init_params;
+  parse_parameters(&init_params, argc, argv);
+
+  // spawn another process to run the container 
   auto stack = malloc(kStackSize);
   auto clone_flags = CLONE_NEWUTS | CLONE_NEWPID | CLONE_NEWNS;
   auto flags = clone_flags | SIGCHLD;
-  auto pid = clone(container_process, (char*)stack + kStackSize, flags, nullptr);
+  auto pid = clone(container_process, (char*)stack + kStackSize, flags, &init_params);
+
   if (pid < 0) {
-    std::printf("Failed to create child process");
+    std::printf("Failed to create child process\n");
     exit(1);
   }
   if (pid > 0) {
